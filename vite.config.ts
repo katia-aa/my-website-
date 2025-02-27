62fe604b93894770f68c8628dec07f6716a2c8ee
Katia Al-Amir <katia.amir@gmail.com>
2025-02-27 16:08:00 -0500

    set correct base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/my-website", // Set correct base path
+  base: "/", // Set correct base path
   plugins: [react()],
   build: {
     outDir: "dist", // Output directory
